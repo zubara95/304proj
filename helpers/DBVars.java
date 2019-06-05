0aa9abb0634e0703d2db03a4d12a534a8bbc9f53
wmatous <willmatous@gmail.com>
2019-06-05 16:00:25 -0700

    jdbc

---
+++ helpers/DBVars.java
@@ -0,0 +1,5 @@
+package helpers;
+public class DBVars  {
+    public static final String username = "";
+    public static final String password = "";
+}
--- javaserver.java
+++ javaserver.java
@@ -1,24 +1,43 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.sql.*;
+import static helpers.DBVars.username;
+import static helpers.DBVars.password;
 
 class TCPServer {
+
+private static Connection con;
+
+
  public static void main(String argv[]) throws Exception {
-  char[] clientSentence= new char[1000];
+  char[] clientSentence= new char[10000];
   String capitalizedSentence;
   ServerSocket welcomeSocket = new ServerSocket(6789);
 
   while (true) {
   try{
-  System.out.println("starting");
    Socket connectionSocket = welcomeSocket.accept();
    BufferedReader inFromClient =
     new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
    DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-   while (inFromClient.ready() && (inFromClient.read(clientSentence, 0, 1000) != -1)) {
-   System.out.println(new String(clientSentence).split(" ", 3)[1]);
-   System.out.println(new String(clientSentence));
+   String urlPath;
+   URL requestURL;
+   String[] parsedPath = new String[]{""};
+   Map<String, String> urlParams = new HashMap<String, String>();
+   while (inFromClient.ready() && (inFromClient.read(clientSentence, 0, 10000) != -1)) {
+        urlPath = new String(clientSentence).split(" ", 3)[1];
+
+        requestURL = new URL("http://localhost:6789"+urlPath);
+        parsedPath = requestURL.getPath().split("/");
+        
+        urlParams = getQueryMap(requestURL.getQuery());
+        System.out.println(urlParams);
+
    }
-   System.out.println("responding");
+   System.out.println("connecting");
+   System.out.println(connect());
+   String response = dispatch(urlParams, parsedPath);
    PrintWriter pw = new PrintWriter(outToClient);
    		pw.print("HTTP/1.1 200 \r\n"); // Version & status code
         pw.print("Content-Type: application/json\r\n"); // The type of data
@@ -28,14 +47,101 @@ class TCPServer {
         pw.print("Allow: OPTIONS, GET, HEAD, POST\r\n");
         pw.print("Connection: close\r\n"); // Will close stream
         pw.print("\r\n"); // End of headers
-		pw.println("{\"id\":1,\"method\":\"object.deleteAll\",\"params\":[\"subscriber\"]}");
+		pw.println(response);
 		pw.flush();
 		pw.close();	
-		System.out.println("finished");
-   // fetch('http://localhost:6789').then((res) => res.json()).then(data => console.log(data)).catch(err => console.error(err));
+   // fetch('http://localhost:6789/path/?params').then((res) => res.json()).then(data => console.log(data)).catch(err => console.error(err));
    }catch(Exception e){
    	System.out.println(e);
    }
   }
  }
+    private static String dispatch(Map<String, String> queryParams, String[] urlPath){
+        String response= "";
+        if(urlPath.length != 0){ // urlPath[0] will be entity type eg account, skill, posting etc
+            switch(urlPath[0]){
+                case "account":
+                    handleAccount(queryParams, urlPath); // urlPath[1] will be null or action like create, update, delete
+                break;
+
+                // add entries here for each database entity type
+            }
+        }
+        return response;
+    }
+
+    // add a handler method here for each type
+
+    private static String handleAccount(Map<String, String> queryParams, String[] path){
+
+        //if (action == "update"){
+
+        //}
+        return "";
+    }
+
+
+
+
+
+    
+    private static Map<String, String> getQueryMap(String query)  
+{  
+    Map<String, String> map = new HashMap<String, String>(); 
+    if (query == null){
+        return map;
+    }
+    String[] params = query.split("&");  
+    
+    if (params == null){
+        return map;
+    }
+    for (String param : params)  
+    {  
+        String name = param.split("=")[0];  
+        String value = param.split("=")[1];  
+        map.put(name, value);  
+    }  
+    return map;  
+}
+/*
+     * connects to Oracle database named ug using user supplied username and password
+     */ 
+    private static boolean connect()
+    {
+      String connectURL = "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug"; 
+
+        try 
+      {
+	// Load the Oracle JDBC driver
+		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+		// may be oracle.jdbc.driver.OracleDriver as of Oracle 11g
+      }
+      catch (SQLException ex)
+      {
+	System.out.println("Message: " + ex.getMessage());
+	System.exit(-1);
+      }
+
+      try 
+      {
+          System.out.println("\nConnecting to Oracle!");
+          System.out.println(username);
+          System.out.println(password);
+          
+	con = DriverManager.getConnection(connectURL,username,password);
+
+	System.out.println("\nConnected to Oracle!");
+	return true;
+      }
+      catch (SQLException ex)
+      {
+	System.out.println("Message: " + ex.getMessage());
+	return false;
+      }
+    }
+
+
+
+
 }
